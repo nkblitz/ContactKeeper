67b0cc61e68a00ccf025063d84019c75db5d135b
Nagaraj Kulkarni <Nagaraj.Kulkarni@zapcg.com>
2020-07-05 18:59:56 +0530

    deployment config

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path'); 
 const express = require('express');
 const connectDB = require('./config/db');
 
@@ -16,6 +17,12 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => (console.log(`Server started at ${PORT}`)));
 
